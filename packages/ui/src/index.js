79ad0d2db219b87a4f9bab8f7b77e16fb51309af
brye <bryewalks@gmail.com>
2023-12-21 15:19:27 -0600

    Added react-intl

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import App from './App'
+import messages from './languages'
 import { store } from 'store'
 import { createRoot } from 'react-dom/client'
 
@@ -12,22 +13,27 @@ import { Provider } from 'react-redux'
 import { SnackbarProvider } from 'notistack'
 import ConfirmContextProvider from 'store/context/ConfirmContextProvider'
 import { ReactFlowContext } from 'store/context/ReactFlowContext'
+import { IntlProvider } from 'react-intl'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
+const locale = navigator.language
+const language = locale.split(/[-_]/)[0]
 
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <SnackbarProvider>
-                    <ConfirmContextProvider>
-                        <ReactFlowContext>
-                            <App />
-                        </ReactFlowContext>
-                    </ConfirmContextProvider>
-                </SnackbarProvider>
-            </BrowserRouter>
-        </Provider>
+        <IntlProvider locale={locale} messages={messages[language]}>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <SnackbarProvider>
+                        <ConfirmContextProvider>
+                            <ReactFlowContext>
+                                <App />
+                            </ReactFlowContext>
+                        </ConfirmContextProvider>
+                    </SnackbarProvider>
+                </BrowserRouter>
+            </Provider>
+        </IntlProvider>
     </React.StrictMode>
 )
---
+++ packages/ui/src/languages/index.js
@@ -0,0 +1,7 @@
+import en_messages from './en.json'
+
+const messages = {
+    en: en_messages
+}
+
+export default messages
